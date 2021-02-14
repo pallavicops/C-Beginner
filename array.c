9fefa95bfd3f2c64acd007f9d9dc426a9ecd22a6
Pallavi Kumari <pallavicops@outlook.com>
2021-02-14 19:33:43 +0530

    Update array.c

--- array.c
+++ array.c
@@ -1,16 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 
 void main()
 {
     int array1[10];
-    int sum=0;
-    for(int i=0;i<10;i++){
-        scanf("%d",&array1[i]);
+    int sum = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        scanf("%d", &array1[i]);
     }
-   for(int i=0;i<10;i++){
-       sum = sum + array1[i];
-        
+    for (int i = 0; i < 10; i++)
+    {
+        sum = sum + array1[i];
     }
-
-    printf("%d",sum);
+    //show the sum
+    printf("%d", sum);
 }
